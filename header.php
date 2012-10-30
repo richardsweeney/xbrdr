de406d1a56a50e380f5229f066b11e1dbbc095a0
Richard Sweeney <richardsweeney@Richards-MacBook-Pro.local>
2012-10-30 17:55:51 +0100

    New, improved header template

--- header.php
+++ header.php
@@ -1,112 +1,41 @@
-<!DOCTYPE html">
-<html <?php language_attributes(); ?>>
-<head>
-
-	<title>Richard Sweeney <?php wp_title(':'); ?></title>
-	<meta name="description" content="I'm a web developer that loves WordPress, HTML5, CSS3, PHP, JavaScript, jQuery.">
-	<!-- <meta name="author" content="Richard Sweeney"> -->
-	<meta name ="viewport" content ="initial-scale=1.0 width=device-width">
-	<link rel="shortcut icon" href="<?php echo IMG; ?>/favicon.ico">
-
-	<?php wp_head(); ?>
-
-</head>
-
-<body <?php body_class(); ?>>
-
-	<div class="header clearfix">
-
-		<div class="header-container center-layout">
-
-			<img src="<?php header_image(); ?>" alt="<?php echo bloginfo('name'); ?>" />
-
-			<header>
-
-				<hgroup>
-				</hgroup>
-
-				<nav role="navigation">
-
-				</nav>
-
-			</header>
-
-		</div>
-
-	</div>
-
 <!DOCTYPE html>
-<html lang="sv">
+<html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<title>XP1 - CrossBorder - Moving Cargo Through Innovation</title>
+		<title><?php echo bloginfo('name'); ?> <?php wp_title('-'); ?></title>
+		<link rel="shortcut icon" href="<?php echo IMG; ?>/favicon.ico">
 		<meta name="description" content="" />
 		<meta name="author" content="Emma Larsson, ID kommunikation ab" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+		<?php wp_head(); ?>
+
 		<!--css-->
-		<link rel="stylesheet" type="text/css" href="../assets/styles/bootstrap.css" />
-		<link rel="stylesheet" type="text/css" href="../assets/styles/bootstrap-responsive.css" />
-		<link rel="stylesheet" type="text/css" href="../assets/styles/styles.css" />
 		<!--stylefix for ie-->
 		<!--[if lt IE 10]>
 		<style type="text/css">
-
-		.navbar-inner  {
-		filter: none;
-		}
-		.navbar-white .nav > .active > a {
-		filter: none;
-		}
-		#page {
-		margin-top: 120px;
-		max-width: 900px;
-		}
-		.container, .navbar-static-top .container, .navbar-fixed-top .container, .navbar-fixed-bottom .container {
-		max-width: 980px;
-		}
-		.navbar-white .brand {
-		display: block;
-		float: left;
-		padding: 15px 20px;
-		margin-left: -20px;
-		font-size: 20px;
-		font-weight: 200;
-		color: rgb(119, 119, 119);
-		text-shadow: 0px 1px 0px rgb(255, 255, 255);
-		}
-		.navbar-white .nav {
-		font-size: 14px;
-		font-weight: 600;
-		margin-top: 35px;
-		float: right;
-		}
-		.navbar-white .nav li {
-		line-height: 61px;
-		}
-		.below-header {
-		height: 13px;
-		width: 100%;
-		margin-top:1px;
-		}
+			.navbar-inner  { filter: none; }
+			.navbar-white .nav > .active > a { filter: none; }
+			#page { margin-top: 120px; max-width: 900px; }
+			.container, .navbar-static-top .container, .navbar-fixed-top .container, .navbar-fixed-bottom .container { max-width: 980px; }
+			.navbar-white .brand { display: block; float: left; padding: 15px 20px; margin-left: -20px; font-size: 20px; font-weight: 200; color: #707070; }
+			.navbar-white .nav { font-size: 14px; font-weight: 600; margin-top: 35px; float: right; }
+			.navbar-white .nav li { line-height: 61px; }
+			.below-header { height: 13px; width: 100%; margin-top:1px;}
 		</style>
 		<![endif]-->
 		<!-- JS for IE -->
 		<!--[if lt IE 9]>
-
-		<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
-
-		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
-
-		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-
+			<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+			<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+			<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
-
-		<script type="text/javascript">
+		<script>
 			var _gaq = _gaq || [];
 			_gaq.push(['_setAccount', 'UA-35754072-1']);
 			_gaq.push(['_trackPageview']);
-
 			(function() {
 				var ga = document.createElement('script');
 				ga.type = 'text/javascript';
@@ -115,34 +44,31 @@
 				var s = document.getElementsByTagName('script')[0];
 				s.parentNode.insertBefore(ga, s);
 			})();
-
 		</script>
+
 	</head>
-	<body data-spy="scroll" data-target=".navbar">
+	<body data-spy="scroll" data-target=".navbar" <?php body_class(); ?>>
 		<!--[if lt IE 8]><p class=chromeframe>Din webbläsare är <em>föråldrad!</em> <a href="http://browsehappy.com/">Uppgradera till en annan webbläsare</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
 		<div class="navbar navbar-white navbar-fixed-top">
 			<div class="navbar-inner">
 				<div class="container">
-					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a><a class="brand" href="../index.htm" title="CrossBorder"><img src="../assets/images/logotype.png" alt="" /></a>
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+					<a class="brand" href="<?php echo URL; ?>" title="CrossBorder">
+						<img src="<?php header_image(); ?>" alt="<?php echo bloginfo('name'); ?>" />
+					</a>
 					<div class="nav-collapse collapse">
-						<ul class="nav">
-							<li class="active">
-								<a href="XP1.html" title="Produkter">Produkter</a>
-							</li>
-							<li>
-								<a href="../testcenter/testcenter.html" title="Testcenter">Testcenter</a>
-							</li>
-							<li>
-								<a href="../om-oss/om-oss.html" title="Om oss">Om oss</a>
-							</li>
-							<li>
-								<a href="../kontakt/kontakt.html" title="Kontakt">Kontakt</a>
-							</li>
-						</ul>
+						<?php rps_print_main_navigation() ?>
 					</div>
-					<!--/.nav-collapse -->
+
+					<?php if (is_front_page()): ?>
+						<?php rps_print_parallax_navigation(); ?>
+        	<?php endif; ?>
+
 				</div>
 				<div class="below-header"></div>
 			</div>
 		</div>
-		<!--/ .navbar+shadows -->
